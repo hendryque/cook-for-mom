a485fea3cf2d675a12a2a12b04484c2d8615f8b3
@anulman <anulman@gmail.com>
2018-03-13 12:09:38 -0400

    PARALLAX

--- app/pods/index/controller.js
+++ app/pods/index/controller.js
@@ -1,8 +1,6 @@
 import Controller, { inject as controller } from '@ember/controller';
 import { validator, buildValidations } from 'ember-cp-validations';
 
-import BREAKPOINTS from '../../utils/breakpoints';
-
 const Validations = buildValidations({
   email: [
     validator('presence', true),
@@ -14,19 +12,5 @@ export default Controller.extend(Validations, {
   application: controller(),
 
   email: '',
-  isHeroFormShowing: true,
-
-  computeParallaxBackgroundStyle
+  isHeroFormShowing: true
 });
-
-function computeParallaxBackgroundStyle(element) {
-  let isBootstrapMdDown = window.innerWidth < BREAKPOINTS.md;
-  let heightMultiplier = isBootstrapMdDown ? 1.5 : 2;
-  let { height } = element.nextElementSibling.getBoundingClientRect();
-
-  element.style.top = `${-0.25 * height}px`;
-  element.style.height = `${heightMultiplier * height}px`;
-  element.style['background-position'] = isBootstrapMdDown ?
-    'bottom center' :
-    'center';
-}
